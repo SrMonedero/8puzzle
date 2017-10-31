5380902d85cbc587d200bda29fd51cdb800caa7a
Quique <quiquealvarezmonedero@gmail.com>
2017-10-31 15:40:08 +0100

    Instantiate pieces on initial positions

--- Assets/Scripts/PuzzleControllerBehaviour.cs
+++ Assets/Scripts/PuzzleControllerBehaviour.cs
@@ -3,4 +3,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PuzzleControllerBehaviour : MonoBehaviour {}
+public class PuzzleControllerBehaviour : MonoBehaviour {
+	private readonly int numOfPieces = 8;
+	public GameObject piece;
+	public Vector2[] positions;
+
+	void Start() {
+		for (int i = 0; i < numOfPieces; i++) {
+			Vector2 position = positions[i];
+			Instantiate(piece, new Vector3(position.x, position.y, 0), Quaternion.identity);
+		}
+	}
+}
